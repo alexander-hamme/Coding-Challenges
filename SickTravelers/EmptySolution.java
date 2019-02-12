262c96d0f0d1d6b88a4992063ea329b08986195f
Alexander Hamme <ah2166@bard.edu>
2019-02-12 17:11:37 -0500

    Update EmptySolution.java

--- SickTravelers/EmptySolution.java
+++ SickTravelers/EmptySolution.java
@@ -16,11 +16,11 @@ class EmptySolution {
 
 	public static void main(String[] args) {
 
-		travelers = new String[]{
-		        "Lisa HEALTHY London Tokyo Seattle Berlin Tokyo",
-			"Chris SICK Berlin Tokyo London Seattle Berlin",
-			"Mary SICK Seattle London Tokyo Berlin",
-			"Daren HEALTHY Berlin Berlin London Seattle",
+		String[] travelers = {   // 8 steps
+			"David HEALTHY London Tokyo Berlin Tokyo",
+			"Lisa RECOVERING Tokyo Berlin London Seattle Berlin",
+			"Frank SICK Seattle London Tokyo Berlin",
+			"Mary SICK Berlin Berlin London Seattle"
 		};
 		System.out.println("Steps required: " + solve(travelers));
 	}
