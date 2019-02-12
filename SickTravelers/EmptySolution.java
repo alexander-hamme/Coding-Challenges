91ff93c0341f6b2b3374f8c4e93d6e1cebb6e8c3
Alexander Hamme <ah2166@bard.edu>
2019-02-12 00:49:36 -0500

    Update and rename MySolution.java to EmptySolution.java

---
+++ SickTravelers/EmptySolution.java
@@ -0,0 +1,26 @@
+class EmptySolution {
+
+
+	public static int solve(String[] inputLines) {
+
+		// your code goes here
+
+	}
+
+
+
+
+
+
+
+	public static void main(String[] args) {
+
+		travelers = new String[]{
+		        "Lisa HEALTHY London Tokyo Seattle Berlin Tokyo",
+			"Chris SICK Berlin Tokyo London Seattle Berlin",
+			"Mary SICK Seattle London Tokyo Berlin",
+			"Daren HEALTHY Berlin Berlin London Seattle",
+		};
+		System.out.println("Steps required: " + solve(travelers));
+	}
+}
