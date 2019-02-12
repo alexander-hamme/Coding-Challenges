a152e58141d1c80bf385b853cb606e2db44f815e
Alexander Hamme <ah2166@bard.edu>
2019-02-12 17:07:38 -0500

    Update EmptySolution.java

--- SickTravelers/EmptySolution.java
+++ SickTravelers/EmptySolution.java
@@ -1,14 +1,15 @@
 class EmptySolution {
 
-
+	// online interview coding challenges are often given on a website like HackerRank.
+	// In Java, your solution typically must go inside an already created "Solution" class,
+	// and the function that gets called is usually static, which is how I've set it up here.
+	
+	
 	public static int solve(String[] inputLines) {
 
 		// your code goes here
 
 	}
-
-
-
 
 
 
